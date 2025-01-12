880a336b58949c285e507d05ff793f85e1b8a2d1
IsaevMansur <stevenusmanso@gmail.com>
2025-01-12 20:17:57 +0300

    upgrade search

--- Sources/Services/UserService.cs
+++ Sources/Services/UserService.cs
@@ -13,7 +13,8 @@ public class UserService : IUserService
         ArgumentNullException.ThrowIfNull(userModel);
 
         if (!ValidationHelper.IsValidEmail(userModel.Email))
-            throw new ArgumentException($"Email is not valid:{userModel.Email} sample: {ValidationHelper.EmailSample}.");
+            throw new ArgumentException(
+                $"Email is not valid:{userModel.Email} sample: {ValidationHelper.EmailSample}.");
         if (!ValidationHelper.IsValidNumber(userModel.PhoneNumber))
             throw new ArgumentException(
                 $"Phone is not valid:{userModel.PhoneNumber} sample: {ValidationHelper.PhoneNumberSample}.");
@@ -24,8 +25,22 @@ public class UserService : IUserService
 
     public IUserModel GetUser(int id)
     {
-        return _users.FirstOrDefault(u => u.Id == id)
-               ?? throw new KeyNotFoundException($"User with Id: {id} not found.");
+        int start = 0;
+        int end = _users.Count - 1;
+        IUserModel? target = null;
+
+        while (start <= end)
+        {
+            int mid = (start + end) / 2;
+            if (_users[mid].Id == id)
+                target = _users[mid];
+            if (_users[mid].Id > id)
+                end = mid - 1;
+            else
+                start = mid + 1;
+        }
+
+        return target ?? throw new KeyNotFoundException($"User with Id: {id} not found.");
     }
 
     public IEnumerable<IUserModel> GetAllUsers()

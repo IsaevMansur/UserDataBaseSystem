60dd52dda89bdfc19b838dff5a1a7d98105128d7
IsaevMansur <stevenusmanso@gmail.com>
2025-01-17 23:21:41 +0300

    add interface for repository

---
+++ Sources/Interfaces/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace UserDBService.Sources.Interfaces;
+
+public interface IUserRepository
+{
+    public long Count { get; }
+    public void Add(IUserModel user);
+    public IUserModel? Get(long id);
+    public IEnumerable<IUserModel>? GetAll();
+    public void Remove(long id);
+    public void Update(long id, IUserModel user);
+    public bool Contains(long id);
+}

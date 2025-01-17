7ec2fe5415a423678ebd1d0506f8a967b32bc31b
IsaevMansur <stevenusmanso@gmail.com>
2025-01-17 23:29:26 +0300

    Add: Checking values of field

--- Sources/Models/Dto/Requests/UserDto.cs
+++ Sources/Models/Dto/Requests/UserDto.cs
@@ -1,22 +1,21 @@
-﻿
-
-// ReSharper disable PropertyCanBeMadeInitOnly.Global
+﻿using System.ComponentModel.DataAnnotations;
+using UserDBService.Sources.Utils;
 
 namespace UserDBService.Sources.Models.Dto.Requests;
 
 public class UserDto
 {
-    // [StringLength(50, MinimumLength = 2, ErrorMessage = "First name must be between 2 and 50 characters.")]
-    public required string FirstName { get; set; }
+    [StringLength(50, MinimumLength = 2, ErrorMessage = "First name must be between 2 and 50 characters.")]
+    public required string FirstName { get; init; }
 
-    // [StringLength(50, MinimumLength = 2, ErrorMessage = "Last name must be between 2 and 50 characters.")]
-    public required string LastName { get; set; }
+    [StringLength(50, MinimumLength = 2, ErrorMessage = "Last name must be between 2 and 50 characters.")]
+    public required string LastName { get; init; }
 
-    // [EmailAddress(ErrorMessage = $"Invalid email address format. Valid format {ValidationUtil.EmailSample}")]
-    // [StringLength(50, ErrorMessage = "Email must not exceed 50 characters.")]
-    public required string Email { get; set; }
+    [EmailAddress(ErrorMessage = $"Invalid email address format. Valid format {ValidationUtil.EmailSample}")]
+    [StringLength(50, ErrorMessage = "Email must not exceed 50 characters.")]
+    public required string Email { get; init; }
 
-    // [Phone(ErrorMessage = $"Invalid phone number format. Valid format {ValidationUtil.PhoneSample}")]
-    // [StringLength(10, ErrorMessage = "Phone number must be an integer and not exceed 10 characters.")]
-    public required string Phone { get; set; }
+    [Phone(ErrorMessage = $"Invalid phone number format. Valid format {ValidationUtil.PhoneSample}")]
+    [StringLength(10, ErrorMessage = "Phone number must be an integer and not exceed 10 characters.")]
+    public required string Phone { get; init; }
 }

eff97aada5dd1d00b1b90687e1f33f571c4f74e9
IsaevMansur <stevenusmanso@gmail.com>
2025-01-22 23:25:17 +0300

    Refactor: nothing

--- Sources/Interfaces/IUserRepository.cs
+++ Sources/Interfaces/IUserRepository.cs
@@ -3,10 +3,10 @@
 public interface IUserRepository
 {
     public long Count { get; }
-    public void Add(IUserModel user);
-    public IUserModel? Get(long id);
-    public IEnumerable<IUserModel>? GetAll();
-    public void Remove(long id);
+    public void Create(IUserModel user);
+    public IUserModel? Read(long id);
     public void Update(long id, IUserModel user);
+    public void Remove(long id);
+    public IEnumerable<IUserModel>? GetAll();
     public bool Contains(long id);
 }

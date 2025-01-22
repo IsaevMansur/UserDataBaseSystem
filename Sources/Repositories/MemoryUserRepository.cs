f817f8c8b42484a5459b4611daa582a6f3fe8617
IsaevMansur <stevenusmanso@gmail.com>
2025-01-22 23:26:18 +0300

    Refactor: Methods names

--- Sources/Repositories/MemoryUserRepository.cs
+++ Sources/Repositories/MemoryUserRepository.cs
@@ -8,23 +8,23 @@ public class MemoryUserRepository : IUserRepository
     private long _currentUserId = 1;
     public long Count => _userDb.Count;
 
-    public void Add(IUserModel user)
+    public void Create(IUserModel user)
     {
         user.Id = _currentUserId++;
         _userDb.Add(user.Id, user);
     }
 
-    public IUserModel Get(long id) => _userDb[id];
-
-    public bool Contains(long id) => _userDb.ContainsKey(id);
-
-    public IEnumerable<IUserModel> GetAll() => _userDb.Values;
-
-    public void Remove(long id) => _userDb.Remove(id);
+    public IUserModel Read(long id) => _userDb[id];
 
     public void Update(long id, IUserModel user)
     {
         user.Id = id;
         _userDb[id] = user;
     }
+
+    public void Remove(long id) => _userDb.Remove(id);
+
+    public bool Contains(long id) => _userDb.ContainsKey(id);
+
+    public IEnumerable<IUserModel> GetAll() => _userDb.Values;
 }
